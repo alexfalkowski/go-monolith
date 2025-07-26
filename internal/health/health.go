9e414b43a84fc9fde87c1c3d9ee331bf7438d255
Alex Falkowski <alexrfalkowski@gmail.com>
2025-07-26 11:51:09 +0200

    feat(deps): update github.com/alexfalkowski/go-health/v2 to v2.3.0

---
+++ internal/health/health.go
@@ -0,0 +1,43 @@
+package health
+
+import (
+	"github.com/alexfalkowski/go-health/v2/checker"
+	"github.com/alexfalkowski/go-health/v2/server"
+	echoer "github.com/alexfalkowski/go-monolith/api/echoer/v1"
+	greeter "github.com/alexfalkowski/go-monolith/api/greeter/v1"
+	"github.com/alexfalkowski/go-service/v2/env"
+	"github.com/alexfalkowski/go-service/v2/errors"
+	"github.com/alexfalkowski/go-service/v2/health"
+	"github.com/alexfalkowski/go-service/v2/time"
+)
+
+func register(name env.Name, srv *server.Server, cfg *Config) {
+	d := time.MustParseDuration(cfg.Duration)
+	regs := health.Registrations{
+		server.NewRegistration("noop", d, checker.NewNoopChecker()),
+		server.NewOnlineRegistration(d, d),
+	}
+
+	srv.Register(name.String(), regs...)
+	srv.Register(echoer.Service_ServiceDesc.ServiceName, regs...)
+	srv.Register(greeter.Service_ServiceDesc.ServiceName, regs...)
+}
+
+func httpHealthObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "healthz", "online")
+}
+
+func httpLivenessObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "livez", "noop")
+}
+
+func httpReadinessObserver(name env.Name, server *server.Server) error {
+	return server.Observe(name.String(), "readyz", "noop")
+}
+
+func grpcObserver(server *server.Server) error {
+	return errors.Join(
+		server.Observe(echoer.Service_ServiceDesc.ServiceName, "grpc", "noop"),
+		server.Observe(greeter.Service_ServiceDesc.ServiceName, "grpc", "noop"),
+	)
+}
