36df412921e6b9dcc5a3042b2b7429d1b489bc10
Alex Falkowski <alexrfalkowski@gmail.com>
2025-11-22 09:39:22 +0100

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.206.0
    https://github.com/alexfalkowski/go-service/pull/1975

--- internal/api/client/client.go
+++ internal/api/client/client.go
@@ -8,8 +8,6 @@ import (
 	"github.com/alexfalkowski/go-service/v2/env"
 	"github.com/alexfalkowski/go-service/v2/id"
 	"github.com/alexfalkowski/go-service/v2/telemetry/logger"
-	"github.com/alexfalkowski/go-service/v2/telemetry/metrics"
-	"github.com/alexfalkowski/go-service/v2/telemetry/tracer"
 	"github.com/alexfalkowski/go-service/v2/transport/grpc"
 	"github.com/alexfalkowski/go-service/v2/transport/grpc/limiter"
 	"go.uber.org/fx"
@@ -25,8 +23,6 @@ type Params struct {
 	di.In
 
 	Lifecycle fx.Lifecycle
-	Tracer    *tracer.Tracer
-	Meter     *metrics.Meter
 	ID        id.Generator
 	Client    *client.Config
 	Logger    *logger.Logger
@@ -37,8 +33,7 @@ type Params struct {
 // NewClient for grpc.
 func NewClient(params Params) (*grpc.ClientConn, error) {
 	conn, err := grpc.NewClient(params.Client.Address,
-		grpc.WithClientLogger(params.Logger), grpc.WithClientTracer(params.Tracer),
-		grpc.WithClientMetrics(params.Meter), grpc.WithClientRetry(params.Client.Retry),
+		grpc.WithClientLogger(params.Logger), grpc.WithClientRetry(params.Client.Retry),
 		grpc.WithClientUserAgent(params.UserAgent), grpc.WithClientTimeout(params.Client.Timeout),
 		grpc.WithClientTLS(params.Client.TLS), grpc.WithClientID(params.ID),
 		grpc.WithClientLimiter(params.Limiter),
