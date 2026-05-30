a0b46c993bb674623f4d49a79ef59c0168f9c940
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-05-30 16:53:31 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service/v2 to v2.500.0 (#598)
    https://github.com/alexfalkowski/go-service/releases/tag/v2.500.0

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -1,10 +1,13 @@
 package cmd
 
-import "github.com/alexfalkowski/go-service/v2/cli"
+import (
+	"github.com/alexfalkowski/go-service/v2/cli"
+	"github.com/alexfalkowski/go-service/v2/strings"
+)
 
 // RegisterServer for cmd.
 func RegisterServer(command cli.Commander) {
 	cmd := command.AddServer("server", "Start go-monolith server", Module)
 
-	cmd.AddInput("")
+	cmd.AddConfig(strings.Empty)
 }
